611b2ca94833c8d3c9ff45fa39a0b2754f30ac73
Malik Hacini <juioiplm@gmail.com>
2026-07-06 16:55:00 +0200

    use simple gch profile

--- experiments/networks.py
+++ experiments/networks.py
@@ -112,7 +112,7 @@ Notes :
 default_params = {
     "n_neighbors": (log_neighbors, {"factor": 1}),
     "random_state": 42,  # Used for kmeans initialization. Has negligible effect for spectral methods.
-    "n_init": 1,
+    "n_init": 100,
     "affinity": "precomputed",  # Graph datasets provide adjacency directly.
     "n_it": 1,
     "assign_labels": "kmeans",
@@ -126,7 +126,7 @@ default_params = {
     ),  # Grid search for DI-SIM methods
     "metric_params": {
         "graph_ch": {
-            "filter_coeffs": {2: 0.5, 3: 0.5},
+            "filter_coeffs": {1: 1},
         },
     },  # Grid search for graph-CH optimization
 }
